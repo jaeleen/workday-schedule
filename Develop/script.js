28d7601cb163611835af83c14dba366f7d806d90
jaeleen <jaeleen.pg@gmail.com>
2022-04-03 22:01:13 -0600

    update javascript

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,42 @@
-var m = moment()
+// import moment from "https://cdn.skypack.dev/moment@2.29.1";
 
-console.log(`toString() => ${m.toString()}`)
+const now = moment();
+const currentTime = { text: moment().format("h:00 A"), hour: moment().hour() };
+
+
+// Current day function
+function currentday(){
+    var nowDay = moment();
+    var currentday = document.getElementById("currentDay").innerHTML = nowDay.format('MMMM Do YYYY, h:mm:ss a');
+}
+currentday();
+
+var timeboxes
+function statusChange(){
+    // var timeBox = document.getElementById("timeBox");
+    // var timeBox2 = document.getElementById("timeBox2");
+    
+    // var timeboxes = [timeBox, timeBox2];
+    
+    var nowTime = moment().format("hh:mm a"); 
+    var codetime = document.getElementById("time").innerHTML;
+    // loop!
+    $(".activity-style").each(function(){
+        if(nowTime === codetime){
+            timeBox.className="present";
+        }
+        else if(nowTime < codetime){
+            timeBox.className="past";
+        }
+        else if(nowTime > codetime){
+            timeBox.className="future";
+        } 
+    })
+  
+}
+
+
+statusChange();
+// console.log(currentTime);
+// console.log(timeNow = moment().hour()); 
+
